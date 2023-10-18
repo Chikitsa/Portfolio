71d23a3514c99edc75e0a7db70a952c64b5f2e72
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-18 21:29:07 +0530

    dont

--- server/server.js
+++ server/server.js
@@ -1,54 +1,84 @@
-const express=require('express')
-const mongoose=require('mongoose')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
-const app=express()
-const url='mongodb+srv://chikitsa:MJRnj0HZ2PyIcIDw@cluster0.jtiiuwr.mongodb.net/?retryWrites=true&w=majority'
+const app = express()
+const url = 'mongodb+srv://chikitsa:MJRnj0HZ2PyIcIDw@cluster0.jtiiuwr.mongodb.net/?retryWrites=true&w=majority'
 
 
 app.use(express.json())
-app.listen(4000,()=>{
+app.use(cors())
+app.listen(4000, () => {
     console.log('server connected ')
 })
 
-app.get('/',async(req,res)=>{
-    res.json({message:"server connected"})
+app.get('/', async (req, res) => {
+    res.json({ message: "server connected" })
 })
 
-mongoose.connect(url).then(()=>{
+mongoose.connect(url).then(() => {
     console.log('db connected')
 })
 
 
-const contactSchema=mongoose.Schema({
-    name:{
-        type:String
+const contactSchema = mongoose.Schema({
+    name: {
+        type: String
     },
-    email:{
-        type:String
+    email: {
+        type: String
     },
-    subject:{
-        type:String
+    subject: {
+        type: String
     },
-    message:{
-        type:String
+    message: {
+        type: String
     }
 })
 
-const contactModel=mongoose.model('Contact',contactSchema)
+const contactModel = mongoose.model('Contact', contactSchema)
 
 
 
+const sendEmail = async (name, email, subject, message) => {
+    try {
 
-app.post('/contact',async(req,res)=>{
-    const {name,email,subject,message}=req.body;
-    if(!name || !email || !subject || !message){
-        res.status(404).json({message:"contact submitted succedffuly"})
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+            }
+        });
+
+
+        let info = await transporter.sendMail({
+            from: `"${name}" <${email}>`,
+            to: 'chikitsa.nijai03@gmail.com',
+            subject: subject,
+            text: message,
+            html: `<b>${message}</b>`,
+        });
+
+        console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.post('/contact', async (req, res) => {
+    const { name, email, subject, message } = req.body;
+    if (!name || !email || !subject || !message) {
+        res.status(404).json({ message: "contact submitted succedffuly" })
     }
 
-    const newContact=await contactModel.create({
-        name,email,subject,message
+    const newContact = await contactModel.create({
+        name, email, subject, message
     })
 
-    res.status(200).json({message:"new contact created ",Contact:newContact})
+    await sendEmail(name, email, subject, message)
+    res.status(200).json({ message: "new contact created ", Contact: newContact })
 })
 
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,92 +1,117 @@
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Contact = () => {
-    const [formData,setFormData]=useState({
-        name:'',
-        email:'',
-        subject:'',
-        message:''
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
     })
 
-    
-    const onChange=(e)=>{
-        setFormData({
-            ...formData,
-            [e.target.name]:e.target.value
-        })
+  }
+
+  const SendContact = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+
+      const parseRes = await response.json()
+      if (response.ok) {
+        toast.success('Message sent successfully')
+      } else {
+        toast.error('Message not sent')
+      }
+    } catch (error) {
+      console.error(error.message)
     }
+  }
 
 
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log(formData)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    SendContact()
+  }
+
+
   return (
     <div>
-        <div className="container">
-          <div className="section-title">
-            <h2>Contact</h2>
-            <p>Thank you for visiting my space on the web. Feel free to explore my work and get in touch. I'm here to listen, create, and inspire..</p>
-          </div>
-          <div className="row" data-aos="fade-in">
-            <div className="col-lg-5 d-flex align-items-stretch">
-              <div className="info">
-                <div className="address">
-                  <i className="bi bi-geo-alt"></i>
-                  <h4>Location:</h4>
-                  <p>Arnala,Virar</p>
-                </div>
-                <div className="email">
-                  <i className="bi bi-envelope"></i>
-                  <h4>Email:</h4>
-                  <p>chikitsa.nijai03@gmail.com</p>
-                </div>
-                <div className="phone">
-                  <i className="bi bi-phone"></i>
-                  <h4>Call:</h4>
-                  <p>+85 2522 1411</p>
-                </div>
-                <iframe
-                  title="Google Maps"
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-                  style={{ border: '0', width: '100%', height: '290px' }}
-                  allowFullScreen
-                  loading="lazy"
-                ></iframe>
+      <div className="container">
+        <div className="section-title">
+          <h2>Contact</h2>
+          <p>Thank you for visiting my space on the web. Feel free to explore my work and get in touch. I'm here to listen, create, and inspire..</p>
+        </div>
+        <div className="row" data-aos="fade-in">
+          <div className="col-lg-5 d-flex align-items-stretch">
+            <div className="info">
+              <div className="address">
+                <i className="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Arnala,Virar</p>
               </div>
+              <div className="email">
+                <i className="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>chikitsa.nijai03@gmail.com</p>
+              </div>
+              <div className="phone">
+                <i className="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+85 2522 1411</p>
+              </div>
+              <iframe
+                title="Google Maps"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                style={{ border: '0', width: '100%', height: '290px' }}
+                allowFullScreen
+                loading="lazy"
+              ></iframe>
             </div>
-            <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="name">Your Name</label>
-                    <input type="text" name="name" className="form-control" id="name" required onChange={onChange}/>
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label htmlFor="email">Your Email</label>
-                    <input type="email" className="form-control" name="email" id="email" onChange={onChange} required />
-                  </div>
+          </div>
+          <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="form-group col-md-6">
+                  <label htmlFor="name">Your Name</label>
+                  <input type="text" name="name" className="form-control" id="name" required onChange={onChange} />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="subject">Subject</label>
-                  <input type="text" className="form-control" name="subject" id="subject" onChange={onChange} required />
+                <div className="form-group col-md-6">
+                  <label htmlFor="email">Your Email</label>
+                  <input type="email" className="form-control" name="email" id="email" onChange={onChange} required />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="message">Message</label>
-                  <textarea className="form-control" name="message" rows="10" onChange={onChange} required></textarea>
-                </div>
-                <div className="my-3">
-                  <div className="loading">Loading</div>
-                  <div className="error-message"></div>
-                  <div className="sent-message">Your message has been sent. Thank you!</div>
-                </div>
-                <div className="text-center">
-                  <button type="submit">Send Message</button>
-                </div>
-              </form>
-            </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="subject">Subject</label>
+                <input type="text" className="form-control" name="subject" id="subject" onChange={onChange} required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" name="message" rows="10" onChange={onChange} required></textarea>
+              </div>
+              <div className="my-3">
+                <div className="loading">Loading</div>
+                <div className="error-message"></div>
+                <div className="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div className="text-center">
+                <button type="submit">Send Message</button>
+              </div>
+            </form>
           </div>
         </div>
+      </div>
     </div>
   )
 }
